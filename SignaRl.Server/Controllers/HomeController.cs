d701f4813d5ed3134a82993d3a5a73df238284ef
1039270526@qq.com <1039270526@qq.com>
2021-12-31 10:59:36 +0800

    IHubContext

--- SignaRl.Server/Controllers/HomeController.cs
+++ SignaRl.Server/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
+using SignaRl.Server.Hubs;
 using SignaRl.Server.Models;
 using System;
 using System.Collections.Generic;
@@ -12,10 +14,13 @@ namespace SignaRl.Server.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<MyHub> _hubContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger
+            , IHubContext<MyHub> hubContext)
         {
             _logger = logger;
+            _hubContext = hubContext;
         }
 
         public IActionResult Index()
@@ -32,6 +37,14 @@ namespace SignaRl.Server.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 页面发送消息按钮调用
+        /// </summary>
+        public async void SendMsg()
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", "Controller Action", $"消息：{DateTime.Now.ToShortDateString()}");
         }
     }
 }
